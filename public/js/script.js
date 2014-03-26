e5b23101765a6d3ce57b6e7b435f24e19125c848
H4ris <h4ris@live.fr>
2014-03-26 18:17:56 +0100

    Hide some buttons when edition's off

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,11 @@ $(document).ready(function() {
 
 	$('#tools #editor').click(function() {
 		$(this).toggleClass('switch-on');
-		backNode.editor.editable(backNode.baliseSearch.getList(), true);
+		if($(this).hasClass('switch-on'))
+			$('#tools ul li:not(#open)').hide();
+		else
+			$('#tools ul li:not(#open)').show();
+		backNode.editor.editable(backNode.baliseSearch.getList(), $(this).hasClass('switch-on'));
 	});
 	$('#tools #open').click(function() {
 		backNode.explorer.pick(function(file) {
