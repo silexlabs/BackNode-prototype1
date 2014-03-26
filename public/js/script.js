3189f0ffe050f17b68f1a7090f56a95f2ab216cd
H4ris <h4ris@live.fr>
2014-03-26 16:51:43 +0100

    ADD BackNode's Design

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var backNode = new BackNode($('#iframe').get(0));
 	
 	$('#tools #editor').click(function() {
+		$(this).toggleClass('switch-on');
 		backNode.editor.editable(true, backNode.baliseSeach.getList());
 	});
 	$('#tools #open').click(function() {
@@ -10,24 +11,37 @@ $(document).ready(function() {
 				backNode.file = file;
 				$(backNode.iframe).attr('src', file.url);
 			} else {
-				alert('Le type de fichier est invalide !');
+				alert('Invalid extension !');
 			}
 		});
 	});
 	$('#tools #save').click(function() {
 		if(backNode.file === null) {
-			alert('Vous n\'avais pas sélectioné de fichier');
+			alert('No file chosen !');
 			return;
 		}
 		backNode.editor.editable(false, backNode.baliseSeach.getList());
 		backNode.explorer.save(backNode.file.url, $(backNode.document).html());
 		backNode.editor.editable(true, backNode.baliseSeach.getList());
 	});
-	
 	$(window).resize(function() {
 		$('#iframe').width($(window).width()-$('#tools').width());
 		$('#tools').height($(window).height());
 		$('#iframe').height($(window).height());
 		$('#CE').css({ marginLeft: -($('#CE').width()/2), marginTop: -($('#CE').height()/2) });
+		var bgrHeight = $(window).height() - $('#menu-wrapper').height() - 110;
+		if(bgrHeight > 200) {			
+			$('#tools #background').stop().fadeIn().css({
+				backgroundImage: "url('../img/backnode.png')",
+				height: bgrHeight
+			});
+		} else if(bgrHeight < 50) {
+			$('#tools #background').stop().fadeOut(50);
+		} else {
+			$('#tools #background').stop().fadeIn().css({
+				backgroundImage: "url('../img/logo.png')",
+				height: 86
+			});
+		}
 	}).resize();
 });
