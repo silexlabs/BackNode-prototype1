77d136b565453c3d9cc811c97e3d0d1048f14acd
unknown <pierre.lecigne@cifacom.com>
2014-03-26 12:12:36 +0100

    merge quantum

--- public/js/backnode.js
+++ public/js/backnode.js
@@ -13,7 +13,84 @@ BackNode.prototype.explorer = {
 
 	}
 };
+BackNode.prototype.editor = {
+    /*This method allow the user to modify the "data-bn" elements if flagEditable is true. It do the contrary if flagEditable is false */
+    editable: function(listEditableContent, flagEditable) {
+        if (flagEditable === true)
+        {
+            $(backNode.document).on('click', "img", function() {
+                backNode.editor.editPicture($(this));
+            });
+            for (key in listEditableContent)
+            {
+                console.log(typeof listEditableContent[key])
+                $(listEditableContent[key]).attr('contenteditable', 'true');
+            }
+        }
+        else
+        {
+          $(backNode.document).off();
+            for (key in listEditableContent)
+            {
+                $(listEditableContent[key]).removeAttr('contenteditable');
+            }
+        }
+    },/* This method allow the user to modify a picture ( alt and src attribute ) */
+    editPicture: function(picture) {
+        showEditableElements();
+        $('#popinPicture').append('<div style="width:500px;height:500px;text-align:center;background:#ddd;position:absolute;left:50%;top:50%;margin:-250px 0 0 -250px"><div style="padding:5px;margin-bottom:20px;background-color:#222;color:#eaeaea;display:block;text-align:right"><span style="cursor:pointer;">Fermer X</span></div><p style="margin-left:20px;margin-bottom:20px;display:inline-block;width:150px;text-align:left;">Picture link</p><p class="backNode-imgSrc" contenteditable="true" style="display:inline-block;margin-left:20px;margin-right:20px;margin-bottom:20px;width:200px;background-color:#fff">' + picture.attr('src') + '</p><div><p style="width:150px;text-align:left;margin-left:20px;margin-bottom:20px;display:inline-block;">Alternative Text</p><p class="backNode-imgSrc" contenteditable="true" style="display:inline-block;margin-left:20px;margin-right:20px;margin-bottom:20px;width:200px;background-color:#fff">' + picture.attr('alt') + '</p></div></div>');
+        $('#popinPicture').slideDown(400, function() {
+            $('#popinPicture').on("click", "span", function() {
+                picture.attr('src', $('.backNode-imgSrc').html());
+                picture.attr('alt', $('.backNode-imgAlt').html());
+                $('#popinPicture').slideUp(400);
+            });
+        });
+    },
+    resizeEditableElements: function(elem) {
+        var top = elem.offset().top;
+        var left = elem.offset().left;
 
+        elem.children('.backnode-editzone').offset({top: top, left: left});
+        elem.children('.backnode-editzone').css({width: elem.width(), height: elem.height()});
+    },
+    getAllEditableElements: function() {
+        $('[data-bn="edit"]').each(function() {
+            backNode.editor.resizeEditableElements($(this));
+        });
+    },
+    showEditableElements: function(){
+        var edit_zone = '<div style="background:#d6ffa0;border:1px solid grey;position:absolute;opacity:0.3" class="backnode-editzone"></div>';
+        $('[data-bn="edit"]').each(function() {
+            $(this).append(edit_zone);
+            $(this).css('z-index', 1);
+            $(this).children('.backnode-editzone').css('z-index', 2);
+        });
+        $('[data-bn="edit"]').click(function() {
+            $('.backnode-editzone').not(this).show;
+        });
+        $(window).resize(function() {
+            backNode.editor.getAllEditableElements();
+        });
+        $('[data-bn="edit"]').keydown(function() {
+            backNode.editor.getAllEditableElements();
+        });
+        $('[data-bn="edit"]').keyup(function() {
+            backNode.editor.getAllEditableElements();
+        });
+        backNode.editor.getAllEditableElements();
+        $('[data-bn="edit"]').css('height', '100%');
+        $('[data-bn="edit"]').mouseenter(function() {
+            $(this).children('.backnode-editzone').hide();
+            //$(this).children('.backnode-editzone').css('z-index', 0);
+        });
+        $('[data-bn="edit"]').mouseleave(function() {
+            if ($(this).is(":focus") === false)
+                $(this).children('.backnode-editzone').show();
+            //$(this).children('.backnode-editzone').css('z-index', 2);
+        });
+    }
+}
 
 
 BackNode.prototype.baliseSearch = {
@@ -103,13 +180,3 @@ BackNode.prototype.baliseSearch = {
 
   }
 };
-
-
-
-
-
-
-
-
-
-
